fd9823829daedc6927f725a6098256af06e6abac
Sam Dunn <samuel.t.dunn@gmail.com>
2018-08-23 12:01:18 -0500

    Tensorflow with titanic data and a working but not useful ensorflow model for north avenue beach.  Time to scale up!

--- titanic tf.R
+++ titanic tf.R
@@ -2,11 +2,14 @@ library(tfestimators)
 library(tidyverse)
 library(titanic)
 
-cols <- feature_columns(
+cols <- feature_columns( #we need to have an idea of what columns are likely going to be important BEFORE we design the model
   column_categorical_with_vocabulary_list("Sex", vocabulary_list = list("male", "female")),
   column_categorical_with_vocabulary_list("Embarked", vocabulary_list = list("S", "C", "Q", "")),
-  column_numeric("Pclass")
+  column_numeric("Pclass"),
+  column_numeric("Age") #I added age, but it made the model's accuracy worse!
 )
+
+
 model <- linear_classifier(feature_columns = cols)
 
 titanic_set <- titanic_train %>%
@@ -20,20 +23,29 @@ titanic_input_fn <- function(data) {
   input_fn(data, 
            features = c("Sex",
                         "Pclass",
-                        "Embarked"), 
+                        "Embarked",
+                        "Age"), 
            response = "Survived")
 }
 
 train(model, titanic_input_fn(train))
 model_eval <- evaluate(model, titanic_input_fn(test))
+
+
 model_eval %>%
   flatten() %>%
   as_tibble() %>%
   glimpse()
 
-tensorboard(model$estimator$model_dir, launch_browser = TRUE)
+#tensorboard(model$estimator$model_dir, launch_browser = TRUE)
 
 model_predict <- predict(model, titanic_input_fn(test))
+saved_model_dir<-model_dir(model)
+
+loaded_model <- linear_regressor(feature_columns = cols,
+                                 model_dir = saved_model_dir)
+loaded_model
+
 tidy_model <- model_predict %>%
   map(~ .x %>%
         map(~.x[[1]]) %>%
